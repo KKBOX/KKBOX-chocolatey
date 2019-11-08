ed12cb686d6f7341c13de284909fdcf575fe6436
poulin <poumason@live.com>
2019-11-08 12:27:54 +0800

    add install folder and some script

---
+++ Install/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'KKBOX'
+$fileType = 'exe'
+$url = 'https://download.kkbox.com/files/KKBOX_Setup-7.1.72.exe'
+
+$silentArgs = '/s'
+$validExitCodes = @(0)
+
+
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "EABC2E751A015FEF9C0346F140F5283E5AC0E1F809227874EC16350C4D38BE24" -checksumType "sha256"
